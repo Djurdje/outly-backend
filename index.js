5eabe36ad45a4fc2c5eb2af7a50c0247c859e544
Djurdje <163303982+Djurdje@users.noreply.github.com>
2026-01-08 19:16:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const { Pool } = require("pg");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -23,33 +24,36 @@ app.get("/", (req, res) => {
 });
 
 // register endpoint
-app.post("/auth/register", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+app.post("/auth/login", async (req, res) => {
+try {
+  const { email, password } = req.body;
 
-    // 1) osnovne validacije
-    if (!email || !password) return res.status(400).send("Missing email or password.");
-    if (password.length < 8) return res.status(400).send("Password too short.");
+  if (!email || !password) return res.status(400).send("Missing email or password.");
 
-    // 2) preveri, če email že obstaja
-    const existing = await pool.query("SELECT id FROM users WHERE email=$1", [email]);
-    if (existing.rows.length > 0) return res.status(409).send("Email already in use.");
+  const result = await pool.query(
+    "SELECT id, email, password_hash FROM users WHERE email=$1",
+    [email]
+  );
 
-    // 3) hash gesla
-    const passwordHash = await bcrypt.hash(password, 12);
+  if (result.rows.length === 0) return res.status(401).send("Invalid credentials.");
 
-    // 4) shrani v bazo
-    await pool.query(
-      "INSERT INTO users (email, password_hash) VALUES ($1, $2)",
-      [email, passwordHash]
-    );
+  const user = result.rows[0];
 
-    // 5) uspeh
-    return res.status(201).send("User created.");
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Server error.");
-  }
+  const ok = await bcrypt.compare(password, user.password_hash);
+  if (!ok) return res.status(401).send("Invalid credentials.");
+
+  const token = jwt.sign(
+    { userId: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
+  );
+
+  return res.status(200).json({ token });
+} catch (err) {
+  console.error(err);
+  return res.status(500).send("Server error.");
+}
+  
 });
 
 // Render nastavi PORT avtomatsko
